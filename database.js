e7b8b03536112bfb8ee179d14812cde86b9dcf3f
turki <trkyable@gmail.com>
2023-06-24 01:41:56 +0300

    addressing the comments

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-let dotenv = require("dotenv").config();
+require("dotenv").config();
 const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_DB_URL);
   console.log(`mongo connected: ${conn.connection.host}`);
